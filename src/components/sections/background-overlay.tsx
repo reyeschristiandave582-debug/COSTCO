067770a90a0929124caf6a0342b7fb9d493675c0
reyeschristiandave582-debug <reyeschristiandave582@gmail.com>
2026-08-05 01:19:33 +0800

    Update background-overlay.tsx

--- src/components/sections/background-overlay.tsx
+++ src/components/sections/background-overlay.tsx
@@ -17,42 +17,42 @@ const AnimatedBackground = () => {
 
       <div className="fixed inset-0 pointer-events-none overflow-hidden opacity-15">
           <Image
-            src="https://i.imgur.com/TRsnqzc.png"
+            src="https://i.imgur.com/VBZsAjk.png"
             alt=""
             width={100}
             height={100}
             className="absolute top-20 left-10 w-17 h-17 object-contain animate-float-gentle delay-1000"
           />
           <Image
-            src="https://i.imgur.com/BEVgljt.png"
+            src="https://i.imgur.com/Jxo7Hj1.png"
             alt=""
             width={160}
             height={160}
             className="absolute top-32 right-10 w-20 h-20 object-contain animate-float-gentle"
           />
           <Image
-            src="https://i.imgur.com/2XNdJOJ.png"
+            src="https://i.imgur.com/1jWavb5.png"
             alt=""
             width={160}
             height={160}
             className="absolute top-1/3 left-20 w-20 h-20 object-contain animate-float-gentle"
           />
           <Image
-            src="https://i.imgur.com/ti3HGz3.png"
+            src="https://i.imgur.com/mmqt9Ed.png"
             alt=""
             width={160}
             height={160}
             className="absolute top-1/1 right-24 w-20 h-20 object-contain animate-float-gentle"
           />
           <Image
-            src="https://i.imgur.com/BNCAQK1.png"
+            src="https://i.imgur.com/gN18fTn.png"
             alt=""
             width={160}
             height={160}
             className="absolute bottom-40 left-16 w-20 h-20 object-contain animate-float-gentle"
           />
           <Image
-            src="https://i.imgur.com/LnG1JRK.png"
+            src="https://i.imgur.com/fx7zmSL.png"
             alt=""
             width={80}
             height={80}
